6a6e3fba528b7f7a78a0587085b70f2001c9e7c7
Alberto Garay <alberto@loquesea>
2021-05-19 13:09:39 +0200

    Candidaturas con AJAX. Falta presentacion javascript del JSON

--- src/main/java/org/agaray/elecciones/controllers/rest/AdminRestController.java
+++ src/main/java/org/agaray/elecciones/controllers/rest/AdminRestController.java
@@ -3,13 +3,24 @@ package org.agaray.elecciones.controllers.rest;
 import java.util.List;
 
 import org.agaray.elecciones.entities.Candidatura;
+import org.agaray.elecciones.repositories.CandidaturaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/rest/admin")
 public class AdminRestController {
-	public List<Candidatura> getCandidaturas() {
-		return null;
+	
+	@Autowired
+	private CandidaturaRepository candidaturaRepository;
+	
+	@GetMapping("candidaturas/{idEleccion}")
+	public List<Candidatura> getCandidaturas(
+			@PathVariable("idEleccion") Long idEleccion
+			) {
+		return candidaturaRepository.findAllByEleccionId(idEleccion);
 	}
 }
--- src/main/java/org/agaray/elecciones/repositories/CandidaturaRepository.java
+++ src/main/java/org/agaray/elecciones/repositories/CandidaturaRepository.java
@@ -1,10 +1,12 @@
 package org.agaray.elecciones.repositories;
 
+import java.util.List;
+
 import org.agaray.elecciones.entities.Candidatura;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CandidaturaRepository extends JpaRepository<Candidatura, Long> {
-
+	public List<Candidatura> findAllByEleccionId(Long idEleccion);
 }
